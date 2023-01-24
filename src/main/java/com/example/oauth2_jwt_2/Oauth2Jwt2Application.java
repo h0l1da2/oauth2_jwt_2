5843a647183b9d25483fd6b737426fd0dd742ffd
h0l1da2 <whaleee@naver.com>
2023-01-25 02:02:54 +0900

    권한 부여 서버와 동일하게 TokenStore 구성 같은 키 값을 이용해서 파싱해야하니 키 값 동일하게 필수

---
+++ src/main/java/com/example/oauth2_jwt_2/Oauth2Jwt2Application.java
@@ -0,0 +1,13 @@
+package com.example.oauth2_jwt_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2Jwt2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2Jwt2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/oauth2_jwt_2/config/ResourceServerConfig.java
@@ -0,0 +1,43 @@
+package com.example.oauth2_jwt_2.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+/**
+ * 권한 부여 서버와 동일하게
+ * 리소스 서버의 TokenStore 구성
+ * 같은 키 값을 이용하는 것이 중요(서명 검증 키)
+ */
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Value("${jwt.key}")
+    private String jwtKey;
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(tokenStore());
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(
+                jwtAccessTokenConverter()
+        );
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        jwtAccessTokenConverter.setSigningKey(jwtKey);
+        return jwtAccessTokenConverter;
+    }
+}
